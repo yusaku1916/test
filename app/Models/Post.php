eeca3d7a31ed98b6120191383b50971a9264bb26
yusaku1916 <hata.yusaku.p1@gmail.com>
2024-03-29 22:15:45 +0900

    カリキュラム08-04

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,7 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
-    public function getPaginateByLimit(int $limit_count = 10)
+    
+    protected $fillable = [
+    'title',
+    'body',
+    ];
+    public function getPaginateByLimit(int $limit_count = 5)
     {
         return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);    
     }
